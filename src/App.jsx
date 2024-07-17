29630525deb3b1f0ee70e53bce41f8de8bf8f9f3
Ömer Faruk Ertürk <erturkomer256@gmail.com>
2024-07-17 15:48:17 +0300

    The first steps in the project have been taken and the app.jsx file has been updated.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <div className="App">
+      <h1>İş İlan Portalı</h1>
+      <div className="header">
+        
+        <div className="arama_kutusu">
+          <input type="text" placeholder="İş İlanı Arayın" />
+          <button id="arama_butonu">Ara</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
