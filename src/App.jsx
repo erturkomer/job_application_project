de22cd5e1fcdec705dd34802e5e772b1f9e3a2c8
Ömer Faruk Ertürk <erturkomer256@gmail.com>
2024-07-30 18:52:49 +0300

    added necessary css and printed job postings to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import IlanList from '../components/IlanList.jsx';
 
 function App() {
 
@@ -7,13 +8,15 @@ function App() {
     <div className="App">
       <h1>İş İlan Portalı</h1>
       <div className="header">
-        
+
         <div className="arama_kutusu">
           <input type="text" placeholder="İş İlanı Arayın" />
           <button id="arama_butonu">Ara</button>
         </div>
 
       </div>
+
+      <IlanList />
     </div>
   )
 }
